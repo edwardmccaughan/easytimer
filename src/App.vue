02a92525d8f94656792ff100776a8cf0313d6ec4
edward mccaughan <edwardmccaughan@gmail.com>
2020-03-31 17:58:03 +0200

    buttons work

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+  background-color: black;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,59 @@
 <template>
   <div>
-    <h1>{{ Math.floor(seconds_remaining / 60) }} : {{ seconds_remaining % 60 }}</h1>
+    <h1 v-if="seconds_remaining > 0">
+      {{ Math.floor(seconds_remaining / 60) }} 
+      :
+      {{ seconds_remaining % 60 }}
+    </h1>
+    <h1 v-else>done!</h1>
+
 
     <div class="times">
-      <button v-on:click="seconds_remaining=10">
-        10s
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=3">
+        3s
       </button>
 
-      <button v-on:click="seconds_remaining=30">
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=30">
         30s
       </button>
 
-      <button v-on:click="seconds_remaining=60">
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=60">
         1m
       </button>
 
-      <button v-on:click="seconds_remaining=180">
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=180">
         3m
       </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=240">
+        4m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=300">
+        5m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=600">
+        10m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=900">
+        15m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=1200">
+        20m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=1500">
+        25m
+      </button>
+
+      <button class="btn btn-primary btn-lg" v-on:click="seconds_remaining=1800">
+        30m
+      </button>
+
+
     </div>
   </div>
 </template>
@@ -35,28 +71,51 @@
         console.log('trying to play...')
         var audio = new Audio(require('./gong.mp3'))
         audio.play()
+      },
+      update_time() {
+        setTimeout(() => {
+          console.log('counting down')
+            if(this.seconds_remaining > 0) {
+              this.seconds_remaining--;
+            } else if (this.seconds_remaining == 0) {
+              this.play_alert() 
+              this.seconds_remaining--;
+            }
+            this.update_time()
+          }, 1000);
       }
     },
-
-    watch: {
-      seconds_remaining: {
-        handler(value) {
-
-          if (value > 0) {
-            setTimeout(() => {
-              this.seconds_remaining--;
-            }, 1000);
-          } else if (value == 0) {
-            this.play_alert() 
-          }
-        },
-        immediate: true // This ensures the watcher is triggered upon creation
-      }
-    }
+    mounted() {
+      this.update_time()
+    },
+    // watch: {
+    //   seconds_remaining: {
+    //     handler(value) {
+    //       if (value > 0) {
+    //         setTimeout(() => {
+    //           this.seconds_remaining--;
+    //         }, 1000);
+    //       } else if (value == 0) {
+    //         this.play_alert() 
+    //       }
+    //     },
+    //     immediate: true // This ensures the watcher is triggered upon creation
+    //   }
+    // }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+h1 {
+  font-size: 7.0em;
+  color: white;
+}
+
+button {
+  font-size: 2em;
+  margin: 0.3em;
+  padding: 0.2em 1.0em;
+}
 </style>
